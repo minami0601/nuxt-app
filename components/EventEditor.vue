1a57a8a4341018b6d32b403bd548b53844db841c
minami0601 <minami.love.bb@icloud.com>
2022-03-15 23:52:51 +0900

    フォーム作成

---
+++ components/EventEditor.vue
@@ -0,0 +1,70 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col>
+        <!-- タイトル入力 -->
+        <v-text-field
+          label="Title"
+          required
+          counter
+          maxlength="25"
+        />
+        <!-- 詳細入力 -->
+        <v-textarea
+          label="Description"
+          counter
+          maxlength="200"
+        />
+      </v-col>
+      <v-col>
+        <v-row>
+          <v-col>
+            <!-- 時刻選択 -->
+            <v-time-picker format="24hr" />
+          </v-col>
+          <v-col>
+            <!-- 日付選択 -->
+            <v-date-picker
+              no-title
+              bottom
+              color="primary"
+            />
+          </v-col>
+        </v-row>
+      </v-col>
+      <v-col>
+        <!-- 日時選択解除 -->
+        <v-list
+          class="overflow-y-auto"
+          max-height="75vh"
+        >
+          <v-list-item align="start">
+            <v-list-item-content>
+              <v-list-item-title>
+                2021/01/01
+              </v-list-item-title>
+              <v-list-item-subtitle>
+                19:00 ~ 21:00
+              </v-list-item-subtitle>
+            </v-list-item-content>
+
+            <v-list-item-action>
+              <v-icon color="accent">
+                mdi-delete
+              </v-icon>
+            </v-list-item-action>
+          </v-list-item>
+        </v-list>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
